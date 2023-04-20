291ff0b51925f3ed9bf0cd5dcdf9c956ee28a354
SaulMoreyra <armhacam21@gmail.com>
2023-04-20 14:02:23 -0600

    feat: add seed crud

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
+import { CarsService } from 'src/cars/cars.service';
+import { BRAND_SEED } from './data/brands.seed';
+import { CARS_SEED } from './data/cars.seed';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
+
+  runSeed() {
+    this.carsService.fillCarsWithSeeData(CARS_SEED);
+    this.brandsService.fillBrandsWithSeeData(BRAND_SEED);
+    return `Seed executed`;
+  }
+}
